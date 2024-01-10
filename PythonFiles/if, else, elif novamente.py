fe43a56b1379ecba4346ffbff700af91ddc0cf0c
maosith <mns.ao@proton.me>
2024-01-10 16:29:49 -0300

    Terceiro programa de if, else e elif

---
+++ PythonFiles/if, else, elif novamente.py
@@ -0,0 +1,35 @@
+nome = input("Qual seu nome? ")
+
+if nome == 'Laura':
+    print("Olá Laura, seja bem vinda ")
+
+elif nome == 'Tomas':
+    print("Acesso parcial")
+
+else:
+    print("Você não é a Laura")
+
+#Outro exemplo
+
+print("Qual número da prateleira você quer remover? ")
+
+num = int(input("Número: "))
+
+if num == 10:
+    print("Você escolheu remover o",num)
+
+elif num == 23:
+    print("Mas você também escolheu o número da sorte, então pode ser também ")
+
+else:
+    print("Poxa, nenhum desses números podem ser retidados da prateleira. ")
+
+#Basicamente o elif é uma alternativa em uma das opções, se não estou errado
+
+
+
+
+
+
+
+
